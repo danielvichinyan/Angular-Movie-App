503bb15835a0989a19a7f550eb8954a65841d9f7
danielvichinyan <45153745+danielvichinyan@users.noreply.github.com>
2020-10-08 13:59:15 +0300

    Added a new jumbotron and removed the search box from the navbar.

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import Movie from '../models/Movie';
 
 const BASE_URL = 'https://api.themoviedb.org/3/movie';
-const IN_THREATRES = 'https://api.themoviedb.org/3/discover/movie'
+const IN_THREATRES = 'https://api.themoviedb.org/3/discover/movie';
 const API_KEY = '?api_key=c99164fa43270779d9e3dc71c2f0ca9c';
 
 @Injectable({
@@ -13,7 +13,9 @@ const API_KEY = '?api_key=c99164fa43270779d9e3dc71c2f0ca9c';
 
 export class MovieService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getPopularMovies(): Observable<Array<Movie>> {
     return this.http.get<Array<Movie>>(BASE_URL + '/popular' + API_KEY);
